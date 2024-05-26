3a13cb3bfdc177a4eadc4ae18510a326b3108fbd
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2024-05-26 12:57:05 -0300

    Quitado algoritmo de la burbuja y modificado algoritmo para calcular la moda

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from scipy.stats import binom
 import matplotlib.pyplot as plt
 
-def ordenarArray(array): 
-    for i in range(len(array) - 1):
-        for j in range(len(array) - i - 1):
-            if array[j] > array[j + 1]:
-                intercambiar(array, j, j + 1)
+def calcular_media(array):
+    sum = 0
+    for i in range(len(array)):
+        sum += array[i]
+    return sum / len(array)
 
-def intercambiar(array, i, j):
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-    
-def calcularMediana(array):
+def calcular_mediana(array):
     if (len(array) % 2 != 0):
         return array[len(array)//2]
     else:
@@ -20,36 +15,42 @@ def calcularMediana(array):
         valor2 = array[len(array)]
         return (valor1 + valor2) / 2
     
-def calcularModa(array):
+def calcular_moda(array):
     max = 0
     current = 0
-    most_repeated = 0
+    results = []
     for i in range(len(array) - 1):
         if array[i] == array[i + 1]:
             current += 1
-        else:
-            if current > max:
-                max = current
-                most_repeated = array[i]
+        if current > max:
+            max = current
+            results.clear()
+        if array[i] != array[i + 1] or i + 1 == len(array) - 1:
+            if current == max:
+                results.append(array[i])
             current = 0
-    return most_repeated
+        if i + 1 == len(array) - 1 and max == 0:
+            results.append(array[i + 1])
+    return results
     
 bin100 = binom.rvs(100, 0.35, size = 100)
 bin1000 = binom.rvs(100, 0.35, size = 1000)
 bin10000 = binom.rvs(100, 0.35, size = 10000)
 bin100000 = binom.rvs(100, 0.35, size = 100000)
 
-# print(bin1)
-# print(bin2)
-# print(bin3)
-# print(bin4)
+bin100.sort()
+bin1000.sort()
+bin10000.sort()
+bin100000.sort()
 
-ordenarArray(bin100)
-ordenarArray(bin1000)
-ordenarArray(bin10000)
-ordenarArray(bin100000)
+print(bin100)
 
-data = [bin100, bin1000, bin10000, bin100000]
+print(calcular_moda([1, 2, 3, 4]))
+print(calcular_media(bin1000))
+print(calcular_media(bin10000))
+print(calcular_media(bin100000))
 
-plt.boxplot(data, vert=False, tick_labels=['BIN 100', 'BIN 1000', 'BIN 10000', 'BIN 100000'])
-plt.show()
+# data = [bin100, bin1000, bin10000, bin100000]
+
+# plt.boxplot(data, vert=False, tick_labels=['BIN 100', 'BIN 1000', 'BIN 10000', 'BIN 100000'])
+# plt.show()

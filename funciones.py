c16cb95e5178cd84d3b3c855eb63a29b60e62e11
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2024-05-26 19:21:35 -0300

    V1.0

---
+++ funciones.py
@@ -0,0 +1,34 @@
+
+def calcular_media(array):
+    sum = 0
+    for i in range(len(array)):
+        sum += array[i]
+    return sum / len(array)
+
+def calcular_mediana(array):
+    if (len(array) % 2 != 0):
+        return array[len(array)//2]
+    else:
+        valor1 = array[(len(array)//2) - 1]
+        valor2 = array[(len(array)//2)]
+        return (valor1 + valor2) / 2
+    
+def calcular_moda(array):
+    if len(array) == 1:
+        return [array[0]]
+    max = 0
+    current = 0
+    results = []
+    for i in range(len(array) - 1):
+        if array[i] == array[i + 1]:
+            current += 1
+        if current > max:
+            max = current
+            results.clear()
+        if array[i] != array[i + 1] or i + 1 == len(array) - 1:
+            if current == max:
+                results.append(array[i])
+            current = 0
+        if i + 1 == len(array) - 1 and max == 0:
+            results.append(array[i + 1])
+    return results
--- main.py
+++ main.py
@@ -1,56 +1,43 @@
 from scipy.stats import binom
 import matplotlib.pyplot as plt
+import statistics as stat
+import numpy as np
+import funciones as f
 
-def calcular_media(array):
-    sum = 0
-    for i in range(len(array)):
-        sum += array[i]
-    return sum / len(array)
+def generar_histograma_binomial(array):
+    plt.hist(array)
+    plt.ylabel(f"Veces que se obtuvo cada resultado al repetir el experimento {len(array)} veces")
+    plt.xlabel("Cantidad de resultados exitosos con distribución binomial donde n = 100")
+    plt.tight_layout()
+    plt.gcf().set_size_inches(6, 6)
+    plt.savefig(f"histograma_binomial_{len(array)}", dpi=150)
+    plt.close()
 
-def calcular_mediana(array):
-    if (len(array) % 2 != 0):
-        return array[len(array)//2]
-    else:
-        valor1 = array[len(array) - 1]
-        valor2 = array[len(array)]
-        return (valor1 + valor2) / 2
-    
-def calcular_moda(array):
-    max = 0
-    current = 0
-    results = []
-    for i in range(len(array) - 1):
-        if array[i] == array[i + 1]:
-            current += 1
-        if current > max:
-            max = current
-            results.clear()
-        if array[i] != array[i + 1] or i + 1 == len(array) - 1:
-            if current == max:
-                results.append(array[i])
-            current = 0
-        if i + 1 == len(array) - 1 and max == 0:
-            results.append(array[i + 1])
-    return results
-    
 bin100 = binom.rvs(100, 0.35, size = 100)
-bin1000 = binom.rvs(100, 0.35, size = 1000)
-bin10000 = binom.rvs(100, 0.35, size = 10000)
-bin100000 = binom.rvs(100, 0.35, size = 100000)
-
 bin100.sort()
+bin1000 = binom.rvs(100, 0.35, size = 1000)
 bin1000.sort()
+bin10000 = binom.rvs(100, 0.35, size = 10000)
 bin10000.sort()
+bin100000 = binom.rvs(100, 0.35, size = 100000)
 bin100000.sort()
 
-print(bin100)
+data = [bin100, bin1000, bin10000, bin100000]
 
-print(calcular_moda([1, 2, 3, 4]))
-print(calcular_media(bin1000))
-print(calcular_media(bin10000))
-print(calcular_media(bin100000))
+plt.boxplot(data, vert=False, tick_labels=['100', '1000', '10000', '100000'])
+plt.ylabel("Cantidad de repeticiones")
+plt.xlabel("Resultados exitosos con distribución binomial donde n = 100 y p = 0.35")
+plt.tight_layout()
+plt.gcf().set_size_inches(8, 5)
+plt.savefig("diagrama_caja_binomial", dpi=150)
+plt.close()
 
-# data = [bin100, bin1000, bin10000, bin100000]
+generar_histograma_binomial(bin100)
+generar_histograma_binomial(bin1000)
+generar_histograma_binomial(bin10000)
+generar_histograma_binomial(bin100000)
 
-# plt.boxplot(data, vert=False, tick_labels=['BIN 100', 'BIN 1000', 'BIN 10000', 'BIN 100000'])
-# plt.show()
+print(f"Media de las 100 muestras con distribición binomial: {np.mean(bin100)}")
+print(f"Media de las 1000 muestras con distribición binomial: {np.mean(bin1000)}")
+print(f"Media de las 10000 muestras con distribición binomial: {np.mean(bin10000)}")
+print(f"Media de las 100000 muestras con distribición binomial: {np.mean(bin100000)}")
